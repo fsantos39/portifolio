47bafe13b62978898b11ff9f477abb667845f2ae
Fernando Santos <f.bsantos2505@gmail.com>
2024-10-17 11:08:56 -0300

    Criando a seção de projetos e o componente de projeto

---
+++ src/components/Projeto/index.tsx
@@ -0,0 +1,13 @@
+import Titulo from '../Titulo'
+import Paragrafo from '../Paragrafo'
+import { Card, LinkBotao } from './styles'
+
+const Projeto = () => (
+  <Card>
+    <Titulo>Projeto Lista de Tarefas</Titulo>
+    <Paragrafo tipo="secundario">Lista de tarefas feita com VueJS</Paragrafo>
+    <LinkBotao>Visualizar</LinkBotao>
+  </Card>
+)
+
+export default Projeto
